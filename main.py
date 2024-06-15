c29418ea66e369980c95b15e57badaa4fde5ca22
Lucas <lucasdearaujo.brandao@gmail.com>
2024-06-15 20:23:47 -0300

    feat: handle with specific folders

--- main.py
+++ main.py
@@ -9,42 +9,57 @@ if API_KEY == "":
     print("could not get API_KEY ")
     exit(1)
 BACKUP_DIR = "./backups"
+FOLDER_TITLE = os.environ.get("FOLDER_TITLE", "")
+HEADERS = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+}
+
+
+def get_dashboard(dashboard: dict):
+    uid = dashboard["uid"]
+    title = dashboard["title"]
+
+    response = requests.get(
+        f"{GRAFANA_URL}/api/dashboards/uid/{uid}", headers=HEADERS)
+    if response.status_code != 200:
+        print(
+            f"Erro ao acionar a rota='{GRAFANA_URL}/api/dashboards/uid/{uid}' http status_code={response.status_code}")
+        raise ConnectionRefusedError
+
+    return response.json(), title, uid
+
+
+def write_json(data: dict, title: str, uid: str):
+    with open(f"{BACKUP_DIR}/{title}_{uid}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json", "w") as f:
+        json.dump(data, f, indent=4)
+    print(f"Backup do dashboard '{title}' concluído.")
 
 
 def backup() -> bool:
-    if not os.path.exists(BACKUP_DIR):
-        os.makedirs(BACKUP_DIR)
+    try:
+        if not os.path.exists(BACKUP_DIR):
+            os.makedirs(BACKUP_DIR)
 
-    headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "Content-Type": "application/json"
-    }
-
-    response = requests.get(f"{GRAFANA_URL}/api/search", headers=headers)
-    if response.status_code != 200:
-        print(
-            f"Erro ao acionar a rota='{GRAFANA_URL}/api/search' http status_code={response.status_code}")
-        return False
-    dashboards = response.json()
-
-    for dashboard in dashboards:
-        uid = dashboard["uid"]
-        title = dashboard["title"]
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-
-        response = requests.get(
-            f"{GRAFANA_URL}/api/dashboards/uid/{uid}", headers=headers)
+        response = requests.get(f"{GRAFANA_URL}/api/search", headers=HEADERS)
         if response.status_code != 200:
             print(
-                f"Erro ao acionar a rota='{GRAFANA_URL}/api/dashboards/uid/{uid}' http status_code={response.status_code}")
-            return False
-        dashboard_details = response.json()
+                f"Erro ao acionar a rota='{GRAFANA_URL}/api/search' http status_code={response.status_code}")
+            raise ConnectionAbortedError
 
-        with open(f"{BACKUP_DIR}/{title}_{uid}_{timestamp}.json", "w") as f:
-            json.dump(dashboard_details, f, indent=4)
-        print(f"Backup do dashboard '{title}' concluído.")
+        dashboards: list[dict] = response.json()
 
-    return True
+        for dashboard in dashboards:
+            if FOLDER_TITLE != "":
+                if dashboard.get("folderTitle"):
+                    dashboard_json, title, uid = get_dashboard(dashboard)
+                    write_json(dashboard_json, title, uid)
+            else:
+                dashboard_json, title, uid = get_dashboard(dashboard)
+                write_json(dashboard_json, title, uid)
+        return True
+    except Exception:
+        return False
 
 
 if __name__ == "__main__":
